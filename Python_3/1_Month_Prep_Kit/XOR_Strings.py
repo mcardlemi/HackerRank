2f0cb12d99c19c3289ffbf3434052dbc1c9c46b6
Michael McArdle <146790048+mcardlemi@users.noreply.github.com>
2023-10-25 22:05:30 -0700

    Create XOR_Strings.py

---
+++ Python_3/1_Month_Prep_Kit/XOR_Strings.py
@@ -0,0 +1,26 @@
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]:
+            res += '0';
+        else:
+            res += '1';
+
+    return res
+
+s = input()
+t = input()
+print(strings_xor(s, t))
+
+
+# In this challenge, the task is to debug the existing code to successfully execute all provided test files.
+
+# Given two strings consisting of digits 0 and 1 only, find the XOR of the two strings.
+
+# To know more about XOR Click Here
+
+# Debug the given function strings_xor to find the XOR of the two given strings appropriately.
+
+# Note: You can modify at most three lines in the given code and you cannot add or remove lines to the code.
+
+# To restore the original code, click on the icon to the right of the language selector.
